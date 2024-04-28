8ff29186e0b29bf0e081b571bf19d4927c9ad0f0
albertdaddy8 <albertslidre@hotmail.com>
2024-04-28 23:52:36 +0200

    la til endringene

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import sqlite3
-import requests
 
 app = Flask(__name__)
 
@@ -21,44 +20,76 @@ cur.execute(""" CREATE TABLE
             todo text
             )""")
 
-brukere = ["Albert", "Mats"]
-
+brukere = ["Albert", "Mats", "Kjell"]
+cur.execute("DELETE FROM brukere")
+con.commit()
 cur.executemany("INSERT INTO brukere (navn) VALUES (?)", [(bruker,) for bruker in brukere])
 con.commit()
 
 @app.route('/get_todos', methods = ["GET"])
 def get_todos():
   id = request.get_json()["id"]
-  cur.execute("SELECT todo.id, todo.todo FROM brukere INNER JOIN todo ON todo.bruker_id = brukere.id WHERE brukere.id = ?", (id,))
+  cur.execute("SELECT todo.ID, todo.todo FROM brukere JOIN todo ON todo.bruker_id = brukere.ID WHERE brukere.ID = ?", (id,))
   data = cur.fetchall()
   content = []
   for todo in data:
-    content.append({"id": todo[0], "todo": todo[1]})
+    print(todo)
+    content.append({"todoId": todo[0], "todo": todo[1]})
   return content
 
-@app.route("/logginn", methods=["GET"])
+@app.route("/logginn", methods=["POST"])
 def logginn():
-    navn = request.get_json()["navn"]
-    cur.execute("SELECT id FROM brukere WHERE navn = ?", (navn,))
-    result = cur.fetchone()
-    if result:
-        bruker_id = result[0]
-        cur.execute("SELECT todo.id, todo.todo FROM brukere INNER JOIN todo ON todo.bruker_id = brukere.id WHERE brukere.id = ?", (bruker_id,))
-        data = cur.fetchall()
-        content = [{"id": todo[0], "todo": todo[1]} for todo in data]
-        return {"Status": "A user", "id": bruker_id, "navn": navn, "todos": content}
+    if request.is_json:
+        navn = request.json.get("navn")
+        cur.execute("SELECT ID FROM brukere WHERE navn = ?", (navn,))
+        result = cur.fetchone()
+        
+        if result:
+            cur.execute("SELECT ID FROM todo")
+            todo_ids = [row[0] for row in cur.fetchall()]
+            return jsonify({"Status": "A user", "id": result[0], "navn": navn, "todo_ids": todo_ids})
+        else: 
+            return jsonify({"Status": "Not a user"}), 404
     else:
-        return {"Status": "Not a user"}
+        return jsonify({"error": "Unsupported Media Type"}), 415
 
 
 @app.route('/leggTilTodo', methods = ["POST"]) 
 def leggTilTodo():
   todo = request.get_json()["todo"]
   id = request.get_json()["id"]
-  cur.execute("INSERT INTO todo(id, todo) VALUES(?,?)", (id, todo))
+  cur.execute("INSERT INTO todo(bruker_id, todo) VALUES(?,?)", (id, todo))
   con.commit()
   return {"Status": "no error"}
     
+
+@app.route('/endreTodo', methods=["GET", "PUT"])
+def endreTodo():
+  if request.method == "GET":
+    id = request.get_json()["id"]
+    cur.execute("SELECT todo.ID, todo.todo FROM brukere INNER JOIN todo ON todo.bruker_id = brukere.ID WHERE todo.ID = ?", (id,))
+    data = cur.fetchone()
+    content = {"id": data[0], "todo": data[1]}
+    return content
+  
+  if request.method == "PUT":
+    id = request.get_json()["id"]
+    todo = request.get_json()["todo"]
+    cur.execute("UPDATE todo SET todo = ? WHERE ID=?", (todo, id))
+    con.commit()
+    return {"Status": "no error"}
+     
+   
+@app.route('/slettTodo', methods=["POST"])
+def slettTodo():
+    data = request.get_json()
+    id = data["id"]
+    todoId = data["todoId"]
+    cur.execute("DELETE FROM todo WHERE bruker_id=? AND ID=?", (id, todoId))
+    con.commit()
+    return {"Status": "no error"}
+
+
 
 
 if __name__ == "__main__":
--- klient/app.py
+++ klient/app.py
@@ -9,33 +9,57 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/logginn", methods=["GET", "POST"])
+@app.route("/logginn", methods=["POST"])
 def logginn():
     if request.method == "POST":
         navn = request.form.get("navn")
         data = {"navn": navn}
-        response = requests.get('http://127.0.0.1:5020/logginn', json=data)
+        response = requests.post('http://127.0.0.1:5020/logginn', json=data)
         try:
             content = response.json()
+            
         except ValueError:
             return render_template("index.html", feil="Ugyldig respons fra serveren")
-
+        
         if content.get("Status") == "A user":
-           
-            return render_template('todo_page.html', data=data)
+            return redirect(url_for('todo_page', data=content["navn"], id=content["id"], todo_id=content["todo_ids"]))
         else:
             return render_template("index.html", feil="Feil brukernavn")
 
 @app.route("/leggTilTodo/<id>", methods = ["POST"])
-def todo_post(id):
-  todo = request.form.get("todo")
+def leggTilTodo(id):
+  todo = request.form.get("todoText")
   requests.post('http://127.0.0.1:5020/leggTilTodo', json={"id": id, "todo": todo})
   return redirect(url_for('todo_page', id=id))
 
+
 @app.route("/todo_page/<id>", methods = ["GET"])
-def home_page(id):
+def todo_page(id):
   data = requests.get('http://127.0.0.1:5020/get_todos', json={"id": id}).json()
+  print(data)
   return render_template("todo_page.html", id=id, data=data)
-    
+
+
+@app.route('/endreTodo/<id>/<todoId>', methods = ["GET", "POST"])
+def endreTodo(id, todoId):
+  if request.method == "GET":
+    data = requests.get('http://127.0.0.1:5020/endreTodo', json={"id": todoId}).json()
+    return render_template('endreTodo.html', id=id, todoId=data["id"], todo=data["todo"])
+  
+  if request.method == "POST":
+    todo = request.form.get("todo")
+    requests.put('http://127.0.0.1:5020/endreTodo', json={"id": todoId, "todo": todo})
+    return redirect(url_for('todo_page', id=id))
+
+
+
+
+@app.route("/slettTodo/<id>/<todoId>", methods=["POST"])
+def slettTodo(id, todoId):
+    requests.post('http://127.0.0.1:5020/slettTodo', json={"id": id, "todoId": todoId})
+    return redirect(url_for('todo_page', id=id))
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5010)
